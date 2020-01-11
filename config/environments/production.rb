f5d70c77f8e8e2167bee8078ad0b7534dc1c1c94
Andre Cesar Gomes <andre270492@gmail.com>
2020-01-11 14:41:11 -0300

    Change production storage service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   # config.active_storage.service = :local
-  config.active_storage.service = :imgur
+  config.active_storage.service = :google
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
