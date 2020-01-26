e13201825ffbf5abe290a3fa965be9136550d578
Andre Cesar Gomes <andre270492@gmail.com>
2020-01-26 14:46:25 -0300

    Add Home and footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,10 @@ document.addEventListener('turbolinks:load', function() {
       $("nav .nav-menu").toggleClass("showing");
   });
 });
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
