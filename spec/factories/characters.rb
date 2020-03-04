d785f5bf15e24cc161db1edb0698c7458548e29c
Andre Cesar Gomes <andre270492@gmail.com>
2020-03-04 11:43:16 -0300

    Add test for new skill

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -5,5 +5,10 @@ FactoryBot.define do
     name { 'Afrodite' }
     constellation
     rank
+
+    trait :with_picture do
+      picture_path = Rails.root.join('spec/support/assets/character-image.png')
+      picture { Rack::Test::UploadedFile.new(picture_path, 'image/png') }
+    end
   end
 end
